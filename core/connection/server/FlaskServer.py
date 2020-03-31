77e5c0f978508978b09320350cd70ad14d84b76a
Dan <magoc@ginasystem.com>
2020-03-31 19:36:13 +0200

    server issue WIP

--- core/connection/server/FlaskServer.py
+++ core/connection/server/FlaskServer.py
@@ -16,13 +16,15 @@ class Server:
         self.UNAUTHORIZED = Response(status=401)
         self.ERROR = Response(status=500)
         self.register_endpoints()
-        self.server.run()
+        self.server.run(port=30)
 
     def register_endpoints(self):
 
         @self.server.route('/device', methods=["POST"])
         def initiate():
             device_config = request.get_json()
+            print("json loaded")
+
             if self.app_manager.register_device(device_config):
                 return self.SUCCESS
             return self.BAD_REQUEST
