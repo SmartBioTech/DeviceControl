fb9e244b969828155c5e294d49c19a46687f08d7
Dan <magoc@ginasystem.com>
2020-04-08 11:19:33 +0200

    reference for gas measure all added to task package

--- custom/tasks/__init__.py
+++ custom/tasks/__init__.py
@@ -1,6 +1,7 @@
-from custom.tasks import PBR
+from custom.tasks import PBR, GAS
 
 classes = {
     "PBR_measure_all": PBR.PBRMeasureAll,
-    "PSI_PBR_pump": PBR.PBRGeneralPump
+    "PSI_PBR_pump": PBR.PBRGeneralPump,
+    "GAS_measure_all": GAS.GASMeasureAll
 }
