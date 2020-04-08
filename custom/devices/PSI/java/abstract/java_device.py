6b28dfe431c95c6a0a066ab526f9c13231b61bec
Dan <magoc@ginasystem.com>
2020-04-08 12:42:48 +0200

    priority argument added but not used

--- custom/devices/PSI/java/abstract/java_device.py
+++ custom/devices/PSI/java/abstract/java_device.py
@@ -36,6 +36,9 @@ class JavaDevice(Connector):
         pass
 
     def get_command_reference(self, cmd_id):
+        print("JAVA SPECIFIC COMMAND REFERENCE GETTER CALLED")
         if not jpype.isThreadAttachedToJVM():
             jpype.attachThreadToJVM()
+            print("JAVA: THREAD ATTACHED TO JVM")
+
         return super(JavaDevice, self).get_command_reference(cmd_id)
