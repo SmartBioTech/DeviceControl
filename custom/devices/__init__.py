7cae9ac54f2811b913cbf6ba0db5892ef78f7a6f
Dan <magoc@ginasystem.com>
2020-05-16 20:19:52 +0200

    device classes updates

--- custom/devices/__init__.py
+++ custom/devices/__init__.py
@@ -1,4 +1,4 @@
-from custom.devices.PSI import scheme, java, test
+from custom.devices.PSI import java, test
 from custom.devices import Phenometrics
 from custom.devices import SEDtronic
 from custom.devices import MettlerToledo
@@ -12,9 +12,6 @@ classes = {
     "PSI_java_PBR": java.PBR,
     "PSI_java_GAS": java.GAS,
     "PSI_java_GMS": java.GMS,
-    "PSI_scheme_PBR": scheme.PBR,
-    "PSI_scheme_GAS": scheme.GAS,
-    "PSI_scheme_GMS": scheme.GMS,
     "test_PBR": test.PBR,
     "test_GAS": test.GAS,
     "test_GMS": test.GMS
