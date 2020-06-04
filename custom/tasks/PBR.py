163a50c051a72f92819c77c4092b013352baf8bc
xtrojak <trojak@mail.muni.cz>
2020-06-04 14:34:17 +0200

    Hotfix: removed ft_channel requirement

--- custom/tasks/PBR.py
+++ custom/tasks/PBR.py
@@ -24,7 +24,6 @@ class PBRMeasureAll(BaseTask):
         self.od_channel = None
         self.tolerance = None
         self.max_outliers = None
-        self.ft_channel = None
         self.latest_values = deque(maxlen=2)
         self.device_id: str = ""
         self.pump_id = None
@@ -42,7 +41,6 @@ class PBRMeasureAll(BaseTask):
             assert self.lower_tol is not None
             assert self.upper_tol is not None
             assert self.od_channel is not None
-            assert self.ft_channel is not None
             assert self.device_id is not ""
 
         except AssertionError:
