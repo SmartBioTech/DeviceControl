ef69f595e7a52586b13321fa7b6b698ddae84bd9
xtrojak <trojak@mail.muni.cz>
2020-10-30 11:11:16 +0100

    Updated DB structure

--- setup/server-database-setup.sql
+++ setup/server-database-setup.sql
@@ -6,7 +6,7 @@ grant all privileges on bioarineo_local.* to 'DeviceControl'@'localhost' identif
 USE bioarineo_local;
 
 
-CREATE TABLE `device` (
+CREATE TABLE `devices` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `class` varchar(100) NOT NULL,
   `type` varchar(100) NOT NULL,
@@ -16,7 +16,18 @@ CREATE TABLE `device` (
 );
 
 
-CREATE TABLE `variable` (
+CREATE TABLE `experiments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `dev_id` int(11) NOT NULL,
+  `start` double NOT NULL,
+  `end` double NOT NULL,
+  `description` varchar(255),
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`dev_id`) REFERENCES `devices`(`id`)
+);
+
+
+CREATE TABLE `variables` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(150) DEFAULT NULL,
   `code` varchar(30) NOT NULL,
@@ -25,32 +36,34 @@ CREATE TABLE `variable` (
 );
 
 
-CREATE TABLE `device_to_variable` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `values` (
+  `id` NOT NULL AUTO_INCREMENT,
+  `time` double NOT NULL,
+  `value` double NOT NULL,
   `dev_id` int(11) NOT NULL,
   `var_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  FOREIGN KEY (`dev_id`) REFERENCES `device`(`id`),
+  FOREIGN KEY (`dev_id`) REFERENCES `devices`(`id`),
   FOREIGN KEY (`var_id`) REFERENCES `variable`(`id`)
 );
 
 
-CREATE TABLE `event` (
+CREATE TABLE `event_types` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `type` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
 );
 
 
-CREATE TABLE `device_event` (
+CREATE TABLE `events` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `dev_id` int(11) NOT NULL,
-  `event_id` int(11) NOT NULL,
+  `event_type` int(11) NOT NULL,
   `time` double NOT NULL,
   `args` varchar(100) NOT NULL,
   `event` varchar(100) NOT NULL,
   `response` varchar(100) NOT NULL,
   PRIMARY KEY (`id`),
-  FOREIGN KEY (`dev_id`) REFERENCES `device`(`id`),
-  FOREIGN KEY (`event_id`) REFERENCES `event`(`id`)
+  FOREIGN KEY (`dev_id`) REFERENCES `devices`(`id`),
+  FOREIGN KEY (`event_type`) REFERENCES `event_types`(`id`)
 );
