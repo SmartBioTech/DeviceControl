b2072378cc2bef747d5cc92260d34a4be25826f6
xtrojak <trojak@mail.muni.cz>
2021-05-06 15:34:33 +0200

    Solved DB IP address for docker image

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -2,3 +2,6 @@
 docker buildx build --output=type=docker --platform linux/arm/v7 -t devicecontrol:arm .
 docker tag devicecontrol:arm bioarineo/devicecontrol:arm
 docker push bioarineo/devicecontrol:arm
+
+# TODO
+# notify Neurons
--- scripts/redeploy.sh
+++ scripts/redeploy.sh
@@ -4,9 +4,7 @@ docker stop $(docker ps -q --filter "name=devicecontrol")
 docker system prune -f
 docker pull bioarineo/devicecontrol:arm
 
-# TODO
-# set local IP to $DATABASE alias
-# hostname -I ?
+DATABASE=$(/sbin/ifconfig eth0 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p')
 
 docker run --privileged -v /dev/serial/by-port/:/dev/serial/by-port/ -p 0.0.0.0:5000:5000 \
        --add-host="database:$DATABASE" --env-file ../DB_CONFIG --name "devicecontrol" bioarineo/devicecontrol:arm
