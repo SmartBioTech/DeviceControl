916602d1a6c1ad3a2af3a8412135f0f225c110f1
xtrojak <trojak@mail.muni.cz>
2021-05-06 16:41:51 +0200

    Added auto startup on reboot or similar event (except manual stop of image)

--- scripts/redeploy.sh
+++ scripts/redeploy.sh
@@ -6,7 +6,7 @@ docker pull bioarineo/devicecontrol:arm
 
 DATABASE=$(/sbin/ifconfig eth0 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p')
 
-docker run -d --privileged -v /dev/serial/by-port/:/dev/serial/by-port/ -p 0.0.0.0:5000:5000 \
+docker run -d --privileged -v /dev/serial/by-port/:/dev/serial/by-port/ -p 0.0.0.0:5000:5000 -restart unless-stopped \
        --add-host="database:$DATABASE" --env-file ../DB_CONFIG --name "devicecontrol" bioarineo/devicecontrol:arm
 
 # TODO
