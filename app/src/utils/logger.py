29047ead79751233933b0f419c796e07f7b65c81
xtrojak <trojak@mail.muni.cz>
2021-05-07 08:39:59 +0200

    Moved log to a separate directory

--- app/src/utils/logger.py
+++ app/src/utils/logger.py
@@ -1,8 +1,11 @@
 import logging
+import os
 
 
 class Logger:
     def __init__(self):
+        if not os.path.exists('log'):
+            os.makedirs('log')
         logging.basicConfig(level=logging.DEBUG,
                             filename="log/device_control.log",
                             format='%(asctime)s %(levelname)s:%(message)s')
