a34159f84e1606b41328f64836adf2284d3de964
xtrojak <trojak@mail.muni.cz>
2021-05-13 17:07:36 +0200

    Preparing scripts for server->Neurons notification

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -1,7 +1,14 @@
 # requires to be logged in with Docker
+git checkout deploy
+git pull
+
 docker buildx build --output=type=docker --platform linux/arm/v7 -t devicecontrol:arm .
 docker tag devicecontrol:arm bioarineo/devicecontrol:arm
 docker push bioarineo/devicecontrol:arm
 
-# TODO
-# notify Neurons
+# notify Neurons, shh IDs must be copied !
+declare -a choice=( $( python3 find_neurons.py ) )
+for port in "${output[@]}";
+do
+  ssh 'bioarineo@localhost' -p "$port" 'cd ~/DeviceControl/scripts && ./redeploy.sh &>/dev/null &'
+done
--- scripts/redeploy.sh
+++ scripts/redeploy.sh
@@ -8,6 +8,3 @@ DATABASE=$(/sbin/ifconfig eth0 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p')
 
 docker run -d --privileged -v /dev/serial/by-port/:/dev/serial/by-port/ -p 0.0.0.0:5000:5000 --restart unless-stopped \
        --add-host="database:$DATABASE" --env-file ../DB_CONFIG --name "devicecontrol" bioarineo/devicecontrol:arm
-
-# TODO
-# location of DB_CONFIG is crucial, make sure exec path is OK !
