91f0b2d23b52f28399c38b55bc05ff5fe2fee04a
xtrojak <trojak@mail.muni.cz>
2021-06-04 09:10:51 +0200

    Fixed test (only config issues)

--- app/__init__.py
+++ app/__init__.py
@@ -29,10 +29,11 @@ def create_app(config_name):
     db.app = app
     db.init_app(app)
 
-    scheduler.init_app(app)
-    scheduler.add_job(func=app_manager.restore_session, id='load_session',
-                      run_date=datetime.now() + timedelta(seconds=30))
-    scheduler.start()
+    if config_name != 'testing':
+        scheduler.init_app(app)
+        scheduler.add_job(func=app_manager.restore_session, id='load_session',
+                          run_date=datetime.now() + timedelta(seconds=30))
+        scheduler.start()
 
     from .core import main as main_blueprint
     app.register_blueprint(main_blueprint)
--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,4 +1,7 @@
 export FLASK_CONFIG="testing"
 export FLASK_ENV="testing"
+export database="localhost"
+export USERNAME=" "
+export PASSWORD=" "
 export FLASK_APP=main.py
 flask test "$1"
