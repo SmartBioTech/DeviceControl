f505fdfec24662be1047b83772c217f04de8e832
xtrojak <trojak@mail.muni.cz>
2021-09-15 09:59:50 +0200

    Hotfix in notify script

--- scripts/notify.sh
+++ scripts/notify.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # notify RPI4 to rebuild
-ssh -p 10909 pi@localhost '~/Downloads/DeviceControl/scripts/rebuild.sh'
+ssh -p 10909 pi@localhost 'cd ~/Downloads/DeviceControl && scripts/rebuild.sh'
 
 # notify Neurons, ssh IDs must be copied !
-declare -a choice=( $( python3 find_neurons.py ) )
-for port in "${output[@]}";
+declare -a ports=( $( python3 ~/DeviceControl/scripts/find_neurons.py ) )
+for port in "${ports[@]}";
 do
   ssh 'bioarineo@localhost' -p "$port" 'cd ~/DeviceControl/scripts && ./redeploy.sh &>/dev/null &'
 done
