f9b234b0ebde5b67e415a4355b65d0963a1e2a6c
Daniel Magoc <daniel.magoc@tietoevry.com>
2022-03-12 18:19:33 +0100

    Abstract task documentation

--- app/src/utils/abstract_task.py
+++ app/src/utils/abstract_task.py
@@ -4,6 +4,12 @@ from .AbstractClass import Interface, abstractattribute
 
 
 class BaseTask(metaclass=Interface):
+    """
+    An interface which every implemented Task must implement.
+    For additional information, see Wiki_.
+
+    .. _Wiki: https://github.com/SmartBioTech/DeviceControl/wiki/New-task
+    """
 
     def __init__(self, config):
         self.is_active = True
@@ -19,12 +25,21 @@ class BaseTask(metaclass=Interface):
 
     @abstractattribute
     def task_id(self):
+        """
+        ID of the task.
+        """
         pass
 
     @abstractmethod
     def end(self):
+        """
+        Terminate the task.
+        """
         pass
 
     @abstractmethod
     def start(self):
+        """
+        Start the task.
+        """
         pass
